117df7da4d12f92518c0ff89822477e444869912
Paruyr <p.muradian@gmail.com>
2022-02-11 18:03:16 +0400

    Initial Commit

---
+++ Vrrrroom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vrrrroom
+//
+//  Created by Paruyr Muradian on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vrrrroom/VrrrroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  VrrrroomApp.swift
+//  Vrrrroom
+//
+//  Created by Paruyr Muradian on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct VrrrroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
