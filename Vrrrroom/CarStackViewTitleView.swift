6ffec67e04623a8b91a9f0615004fb2087f8db53
Paruyr <p.muradian@gmail.com>
2022-02-12 16:04:43 +0400

    Create a UI to display car's title and miles

---
+++ Vrrrroom/CarStackViewTitleView.swift
@@ -0,0 +1,25 @@
+//
+//  CarStackViewTitleView.swift
+//  Vrrrroom
+//
+//  Created by Paruyr Muradian on 2/12/22.
+//
+
+import SwiftUI
+
+struct CarStackViewTitleView: View {
+    var body: some View {
+        HStack(alignment: .center) {
+            Text("Car name")
+            Divider().frame(width: 2.0).background(Color(hex: "A66A53"))
+            Image("notif_gas")
+            Text("120mi")
+        }
+    }
+}
+
+struct CarStackViewTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarStackViewTitleView()
+    }
+}
